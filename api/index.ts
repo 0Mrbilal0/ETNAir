602b798407ef7fe999d3426323df0255bacfbef1
Vincgllmt <vincgllmt02@gmail.com>
2024-12-05 10:21:52 +0100

    feat: :sparkles: added route to doc

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import {router} from './src/Routes/routes';
+import { serve, setup } from 'swagger-ui-express';
 import dotenv from 'dotenv';
+import { parseDocument } from 'yaml';
+import { readFileSync } from 'fs';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/api_doc', serve, setup(parseDocument(readFileSync("./openapi.yml").toString())));
 app.use('/', router)
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
