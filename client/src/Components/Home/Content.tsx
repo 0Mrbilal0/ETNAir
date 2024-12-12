285effb6c1fe59247584770762df505f55aa05ea
Mrbilal <bilal_itchir@Outlook.fr>
2024-12-12 10:50:29 +0100

    co

--- client/src/Components/Home/Content.tsx
+++ client/src/Components/Home/Content.tsx
@@ -31,21 +31,21 @@ function Home() {
             <div className="flex flex-wrap justify-center gap-4">
                 {rentals.map((rental) => (
                     <div key={rental.id}
-                         className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img className="rounded-t-lg" src="/Logo_Lightmode.png" alt=""/>
+                         className="w-72 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href={`/annonces/${rental.id}`}>
+                            <img className="rounded-t-lg max-h-52 w-full object-none" src="/Logo_Lightmode.png" alt=""/>
                         </a>
                         <div className="p-5">
-                            <a href="#">
+                            <a href={`/annonces/${rental.id}`}>
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{rental.title}</h5>
                             </a>
-                            <section className="flex justify-between mb-3">
-                                <p className="text-lg text-gray-900 dark:text-white">{rental.caracteristic.nb_bedroom} chambres </p>
-                                <p className="text-lg text-gray-900 dark:text-white">{rental.nb_person} pers.</p>
-                                <p className="text-lg text-gray-900 dark:text-white">{rental.price}€/nuit</p>
+                            <hr className="mb-4 w-full" />
+                            <section className="flex justify-between mb-16">
+                                <p className="text-sm text-gray-900 dark:text-white">{rental.caracteristic.nb_bedroom} chambres </p>
+                                <p className="text-sm text-gray-900 dark:text-white">{rental.nb_person} pers.</p>
+                                <p className="text-sm text-gray-900 dark:text-white">{rental.price}€/nuit</p>
                             </section>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{rental.type}</p>
-                            <a href="#"
+                            <a href={`/annonces/${rental.id}`}
                                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Reserver
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
